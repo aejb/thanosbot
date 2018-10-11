e5c4d2ef7cb639dc1edfdb2559f600ee1e84a6a7
ash#0001 <thomasbass2001@gmail.com>
2018-10-11 12:16:03 +0100

    Create cog.py

---
+++ cog.py
@@ -0,0 +1,39 @@
+__authors__ = 'electric-blue-green'
+__license__ = 'MIT'
+__status__ = 'Prototype'
+import discord
+from discord.ext import commands
+import sqlite3
+import os
+import subprocess
+import time
+import requests
+import asyncio
+import random
+
+class shellcog:
+    def __init__(self, bot):
+        print(0)
+        self.bot = bot
+        word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+        response = requests.get(word_site)
+        self.wordslist = response.text.splitlines()
+        bot.loop.create_task(self.tomsagayshit())
+
+    async def tomsagayshit(self):
+        print(1)
+        await self.bot.wait_until_ready()
+        print(2)
+        while not self.bot.is_closed():
+            print(3)
+            # do tomsagayshit
+            #print(type(wordslist))
+            #print(random.choice(wordslist))
+            word=(random.choice(self.wordslist))
+            content=("thanos "+word+"\nthanos "+word)
+            await self.bot.get_channel(499898214099582976).send(content)
+            await asyncio.sleep(5)
+
+
+def setup(bot):
+    bot.add_cog(shellcog(bot))
